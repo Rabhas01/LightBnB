6dd789d441e46b92b7f9c4594993254c5abd5931
Rabhas <rabhas.vashisht@gmail.com>
2022-04-08 12:55:30 +0000

    updating getAllproperties function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -104,26 +104,68 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
+
+const getAllProperties = function (options, limit = 10) {
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+      queryParams.push(`%${options.owner_id}%`);
+      queryString += `AND properties.owner_id = $${queryParams.length}`;
+    }
+    
+    if (options.maximum_price_per_night) {
+      queryParams.push(`${options.maximum_price_per_night * 100}`);
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    }
+
+    if (options.minimum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night * 100}`);
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+    }
+    // 4
+    
+    queryString += `
+    GROUP BY properties.id `
+    if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `
+   }
+
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    // 5
+    // console.log(queryString, queryParams);
+  
+    // 6
+    return pool.query(queryString, queryParams).then((res) => {
+      return (res.row);
     })
     .catch((err) => {
-      console.log(err.message);
-    });
-};
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-exports.getAllProperties = getAllProperties;
-
-
+      return(err.message);
+      });
+    
+  };
+  
+    exports.getAllProperties = getAllProperties;
+  
+  
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
